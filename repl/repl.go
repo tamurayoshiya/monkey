3a0ad286a07acf1e1eff23b3ec39ca6733f26344
TAMURA Yoshiya <a@qmu.jp>
2019-02-12 00:41:20 +0900

    update

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/tamurayoshiya/monkey/evaluator"
 	"github.com/tamurayoshiya/monkey/lexer"
 	"github.com/tamurayoshiya/monkey/parser"
 )
@@ -31,8 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
