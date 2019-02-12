0dc6e7ab50b10dc3061fa62e5e862f649293ee6f
TAMURA Yoshiya <a@qmu.jp>
2019-02-13 01:58:01 +0900

    update

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -39,8 +39,9 @@ func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
 	program := p.ParseProgram()
+	env := object.NewEnvironment()
 
-	return Eval(program)
+	return Eval(program, env)
 }
 
 // ------------------------------------------------------------------------
@@ -202,7 +203,7 @@ func TestReturnStatements(t *testing.T) {
 
 // ------------------------------------------------------------------------
 
-// Test return statement
+// Test error handling
 
 func TestErrorHandling(t *testing.T) {
 	tests := []struct {
@@ -244,6 +245,10 @@ func TestErrorHandling(t *testing.T) {
 			`,
 			"unknown operator: BOOLEAN + BOOLEAN",
 		},
+		{
+			"foobar",
+			"identifier not found: foobar",
+		},
 	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
@@ -257,5 +262,24 @@ func TestErrorHandling(t *testing.T) {
 			t.Errorf("wrong error message. expected=%q, got=%q",
 				tt.expectedMessage, errObj.Message)
 		}
+	}
+}
+
+// ------------------------------------------------------------------------
+
+// Test let statement
+
+func TestLetStatements(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let a = 5; a;", 5},
+		{"let a = 5 * 5; a;", 25},
+		{"let a = 5; let b = a; b;", 5},
+		{"let a = 5; let b = a; let c = a + b + 5; c;", 15},
+	}
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
 	}
 }
