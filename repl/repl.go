12547d085344bf59c41e3da0881c3a3413d63e45
TAMURA Yoshiya <a@qmu.jp>
2019-03-06 17:03:06 +0900

    Modify repl for the compiler way

--- repl/repl.go
+++ repl/repl.go
@@ -5,14 +5,58 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/tamurayoshiya/monkey/evaluator"
+	"github.com/tamurayoshiya/monkey/compiler"
 	"github.com/tamurayoshiya/monkey/lexer"
-	"github.com/tamurayoshiya/monkey/object"
 	"github.com/tamurayoshiya/monkey/parser"
+	"github.com/tamurayoshiya/monkey/vm"
 )
 
 const PROMPT = ">> "
 
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+
+		// Prior steps are same as Interpretor,
+		// and heading for posterior, we have
+		// different steps for Compiler
+
+		comp := compiler.New()
+		err := comp.Compile(program)
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Compilation failed:\n %s\n", err)
+			continue
+		}
+
+		machine := vm.New(comp.Bytecode())
+		err = machine.Run()
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Executing bytecode failed::\n %s\n", err)
+			continue
+		}
+
+		stackTop := machine.StackTop()
+		io.WriteString(out, stackTop.Inspect())
+		io.WriteString(out, "\n")
+	}
+}
+
+/*
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 	env := object.NewEnvironment()
@@ -45,6 +89,7 @@ func Start(in io.Reader, out io.Writer) {
 		}
 	}
 }
+*/
 
 func printParserErrors(out io.Writer, errors []string) {
 	for _, msg := range errors {
