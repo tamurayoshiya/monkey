e0281b10185f8dcd1b4c2da0aae6275e9bc2f158
TAMURA Yoshiya <a@qmu.jp>
2019-09-08 23:59:24 +0900

    Implement function call in vm

--- vm/vm.go
+++ vm/vm.go
@@ -163,6 +163,21 @@ func (vm *VM) Run() error {
 			if err != nil {
 				return err
 			}
+		case code.OpCall:
+			fn, ok := vm.stack[vm.sp-1].(*object.CompiledFunction)
+			if !ok {
+				return fmt.Errorf("calling non-function")
+			}
+			frame := NewFrame(fn)
+			vm.pushFrame(frame)
+		case code.OpReturnValue:
+			returnValue := vm.pop()
+			vm.popFrame()
+			vm.pop()
+			err := vm.push(returnValue)
+			if err != nil {
+				return err
+			}
 		}
 	}
 	return nil
